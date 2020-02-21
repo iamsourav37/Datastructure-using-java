06f374594e19538e95166c752d48c4dc2647e85a
iamSourav37 <souravsrk120@gmail.com>
2020-02-21 19:07:15 +0530

    travarsal method (in,pre,post order) defined

--- src/non/linier/ds/tree/BST.java
+++ src/non/linier/ds/tree/BST.java
@@ -75,5 +75,26 @@ public class BST {
         return root;
     }
 
+    public void inOrder(BinarySearchTreeNode root){
+        if(root == null)
+            return;
+        inOrder(root.getLeft());
+        System.out.print(root.getData()+" ");
+        inOrder(root.getRight());
+    }
+    public void preOrder(BinarySearchTreeNode root){
+        if(root == null)
+            return;
+        System.out.print(root.getData()+" ");
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+    }
+    public void postOrder(BinarySearchTreeNode root){
+        if(root == null)
+            return;
+        postOrder(root.getLeft());
+        postOrder(root.getRight());
+        System.out.print(root.getData()+" ");
+    }
 
 }
--- src/non/linier/ds/tree/BSTRunner.java
+++ src/non/linier/ds/tree/BSTRunner.java
@@ -13,30 +13,17 @@ public class BSTRunner {
         root = a.insert(root,19);
         root = a.insert(root,26);
         root = a.insert(root,21);
+        root = a.insert(root,3);
+        root = a.insert(root,23);
+        root = a.insert(root,1);
+        root = a.insert(root,4);
 
-
-        System.out.println(a.findMaximum(root).getData());
-        System.out.println(a.findMinimum(root).getData());
-
-        System.out.println("Root right data : "+root.getRight().getData());
-        // deleting 20
-        root = a.delete(root,20);
-        System.out.println("After deleteing 20, now right data is : "+root.getRight().getData());
-
-        // deleting 19
-        root = a.delete(root,19);
-        System.out.println("After deleteing 19, root right data is : "+root.getRight().getData());
-
-        // deleting 26
-        root = a.delete(root,26);
-        System.out.println("After deleteing 26, maximum value is : "+a.findMaximum(root).getData());
-
-        // deleting 2
-        root = a.delete(root,2);
-        System.out.println("After deleteing 2, minimum value is : "+a.findMinimum(root).getData());
-
-
-        a.delete(root,121);
+        System.out.println("Inorder travarsal : ");
+        a.inOrder(root);
+        System.out.println("\nPreorder travarsal : ");
+        a.preOrder(root);
+        System.out.println("\nPostorder travarsal : ");
+        a.postOrder(root);
 
 
 
